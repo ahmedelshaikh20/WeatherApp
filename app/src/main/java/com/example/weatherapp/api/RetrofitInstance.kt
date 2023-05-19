bfaec78a3f4ee8105cd877832f004866ea5befcf
Ahmed Salah <ahmedelshaikh51@gmail.com>
2023-05-19 19:41:33 +0300

    Moving keys to build config

--- app/src/main/java/com/example/weatherapp/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/weatherapp/api/RetrofitInstance.kt
@@ -1,12 +1,12 @@
 package com.example.weatherapp.api
 
+import com.example.weatherapp.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
-private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 val logging = HttpLoggingInterceptor().apply {
   this.level = HttpLoggingInterceptor.Level.BODY
 };
@@ -17,7 +17,7 @@ var client = OkHttpClient.Builder()
 object RetrofitInstance {
   val api : WeatherApi by lazy {
     Retrofit.Builder()
-      .baseUrl(BASE_URL)
+      .baseUrl(BuildConfig.BASE_URL)
       .addConverterFactory(GsonConverterFactory.create())
       .client(client)
       .build()
--- app/src/main/java/com/example/weatherapp/api/WeatherRepositry.kt
+++ app/src/main/java/com/example/weatherapp/api/WeatherRepositry.kt
@@ -1,6 +1,7 @@
 package com.example.weatherapp.api
 
 import com.example.weatherapp.API_KEY
+import com.example.weatherapp.BuildConfig
 import com.example.weatherapp.model.WeatherApiResponse
 import retrofit2.Response
 
@@ -8,7 +9,7 @@ class WeatherRepositry() {
 
   suspend fun getWeatherByLocation(long : Double, lat :Double): Response<WeatherApiResponse> {
 
-     return RetrofitInstance.api.getCurrentWeather(long, lat, API_KEY, "metric")
+     return RetrofitInstance.api.getCurrentWeather(long, lat, BuildConfig.API_KEY, "metric")
 
    }
 }
